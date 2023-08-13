5b89f19ee112a2187206675447578a8f5e354829
Roger Wang <rogerwang2520@gmail.com>
2023-08-12 22:32:23 -0700

    first commit

---
+++ animexample.py
@@ -0,0 +1,23 @@
+# Source: https://ankurankan.github.io/plotting-and-animating-networkx-graphs.html
+import networkx as nx
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import random
+
+# Graph initialization
+G = nx.Graph()
+G.add_nodes_from([1, 2, 3, 4, 5, 6, 7, 8, 9])
+G.add_edges_from([(1,2), (3,4), (2,5), (4,5), (6,7), (8,9), (4,7), (1,7), (3,5), (2,7), (5,8), (2,9), (5,7)])
+
+# Animation function
+def animate(frame_num):
+    colors = ['r', 'g', 'b', 'c', 'm', 'y', 'k', 'w']  # https://matplotlib.org/stable/gallery/color/named_colors.html
+    nx.draw_circular(G, node_color=[random.choice(colors) for j in range(9)])
+
+fig, ax = plt.subplots()
+plt.axis('off')
+
+# Animator call
+anim = animation.FuncAnimation(fig, animate, frames=5, interval=250, blit=False)
+
+plt.show()  # Display the animation in an interactive window
---
+++ main.py
@@ -0,0 +1,45 @@
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+class Node:
+    """
+    key: unique name shown in the graph
+    val: node value (usually color string)
+    props: tuple of special node properties
+    """
+    def __init__(self, key, val=None, props=()):
+        self.key = key
+        self.val = val
+        self.props = props
+    
+    def __str__(self):
+        return str(self.key)
+    
+    # These allow edges_keys_to_nodes to use node keys as keys
+    def __hash__(self):
+        return hash(self.key)
+    def __eq__(self, other):
+        if isinstance(other, Node):
+            return self.key == other.key
+        return False
+    
+    @staticmethod
+    def edge_list_keys_to_nodes(edges):
+        return [Node.edge_key_to_nodes(edge) for edge in edges]
+    @staticmethod
+    def edge_key_to_nodes(edge):
+        return (Node(edge[0]), Node(edge[1]))
+
+edge_key_list = [('A', 'B'), ('A', 'C'), ('A', 'D'), ('B', 'C'), ('B', 'D'), ('C', 'D')]
+edge_list = Node.edge_list_keys_to_nodes(edge_key_list)
+G = nx.Graph()
+G.add_edges_from(edge_list)
+
+if nx.is_planar(G):
+    nx.draw_planar(G, with_labels=True)
+else:
+    nx.draw_spring(G, with_labels=True)
+
+plt.show()
